d4310af34968a5c1f13c6377eb818991b2599be7
sharippe <sharyf101@gmail.com>
2021-11-05 23:58:58 +0500

    completed html setup

--- index.php
+++ index.php
@@ -1,124 +1,58 @@
 <?php
 
-
-    
-
-/*
-    if ($marks > 74.5) {
-        echo "Grade A";
-    } elseif ($marks > 60) {
-        echo "Grade B";
-    } elseif ($marks > 50) {
-        echo "Grade C";
-    } elseif ($marks > 40) {
-        echo "Grade D";
-    } else {
-        echo "Sorry, You have Failed";
-    }
-
-    
-
-
-    switch ($marks) {
-        case ($marks > 74.5):
-            echo "Grade A";
-            break;
-
-        case ($marks > 60):
-            echo "Grade B";
-            break;
-
-        case ($marks > 50):
-            echo "Grade C";
-            break;
-
-        case ($marks > 40):
-            echo "Grade D";
-            break;
-
-        default :
-            echo "Sorry, You have Failed";
-            break;
-
-    }
-
-    echo "<br/>";
-    echo strlen ("Hello World");
-    echo "<br/>";
-    echo date ('y/m/d');
-    echo "<br/>";
-    echo pi ();    
-
-
-
-
-function printGrade()
-{
-
-    $marks = 42;
-
-    if ($marks > 74.5) {
-        echo "Grade A";
-    } elseif ($marks > 60) {
-        echo "Grade B";
-    } elseif ($marks > 50) {
-        echo "Grade C";
-    } elseif ($marks > 40) {
-        echo "Grade D";
-    } else {
-        echo "Sorry, You have Failed";
-    }
-}
-
-printGrade();
-*/
-
-$marks =    [
-                    "Ali" => [
-                                'Chemistry' => 75,
-                                'English' => 80,
-                                'Maths' => 45,
-                             ],   
-                         
-                "Ahmed" =>   [
-                                'Islam' => 40,
-                                'Maths' => 60,
-                                'Science' => 99,
-                             ], 
-                
-                "fathimath" => [
-                                 'English' => 35,
-                                 'Maths' => 88,
-                               ],       
-                           
-            ];
-
-            function getGrade($marks = 0, $name = 'Ali')
-            {
-            
-            
-                if ($marks > 74.5) {
-                    return "$name got Grade A";
-                } elseif ($marks > 60) {
-                    return "$name got Grade B";
-                } elseif ($marks > 50) {
-                    return "$name got Grade C";
-                } elseif ($marks > 40) {
-                    return "$name got Grade D";
-                } else {
-                    return "$name has Failed";
-                }
-            }
-            
-
-            foreach ($marks as $students => $subjects ) {
-
-                foreach ($subjects as $subject => $mark) {
-                    echo getGrade ($mark, $students)  .  " in  $subject";
-                    echo "<br>";
-                }
-            }
-            printGrade();
-
+    include ('partials/header.php');
+    include ('partials/sidebar.php');
 ?>
+
+
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+
+
+        <h1 class="h2">Dashboard</h1>
+
+
+        <div class="btn-toolbar mb-2 mb-md-0">
+          <div class="btn-group me-2">
+            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+          </div>
+        </div>
+      </div>
+
+      
+      <h2>Section title</h2>
+
+
+
+      <div class="table-responsive">
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+    
+          </tbody>
+        </table>
+      </div>
+    </main>
+  
+
+    <?php
+        include ('partials/footer.php');
+    ?>
 
